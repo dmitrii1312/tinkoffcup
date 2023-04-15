64ae2079e840eb3bc1ee187f3d2104196c69e54e
Alexandr Malyshev <a.malyshev41@gmail.com>
2023-04-15 19:53:22 +0300

    add modify method

--- backend/calendar_zone.py
+++ backend/calendar_zone.py
@@ -48,5 +48,17 @@ class CalendarZone:
         event.delete
         return
 
+    def modify_task(self, event, summary, start, end):
+        if summary:
+            event.icalendar_component["summary"] = summary
+
+        if start:
+            event.icalendar_component["dtstart"].dt = start
+        
+        if end:
+            event.icalendar_component["dtend"].dt = end
+
+        event.save()
+
 obj = CalendarZone("http://tsquared.keenetic.pro:5232/", 'admin', 'admin')
 print(obj.get_existing_cals())
