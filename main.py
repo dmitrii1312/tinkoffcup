3b0e3b425c57eb36fb3c3c24dd3473886934b96e
Alexandr Malyshev <a.malyshev41@gmail.com>
2023-04-15 14:52:41 +0300

    rename methods

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from datetime import datetime, timedelta
 from icalendar import Event
 
 
-def add_event(calendar, start=datetime, end=datetime, summary=""):
+def add_task(calendar, start=datetime, end=datetime, summary=""):
     event = calendar.save_event(
         dtstart=start,
         dtend=end,
         summary=summary,
     )
 
-def get_event(calendar):
+def get_task(calendar):
     return
 
-def del_event(calendar):
+def del_task(calendar):
     return
 
 
@@ -26,5 +26,4 @@ principal = client.principal()
 
 # Получение списка календарей
 calendar = principal.calendar(name="tinkoffcup")
-add_event(calendar, start=datetime(2023, 5, 13, 5), end=datetime(2023, 5, 13, 9), summary="Test event")
-
+add_task(calendar, start=datetime(2023, 5, 13, 5), end=datetime(2023, 5, 13, 9), summary="Test event")
