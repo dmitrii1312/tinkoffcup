da4a728ee4948c8ec44913b1dfdd3d17ad32215b
Alexandr Malyshev <a.malyshev41@gmail.com>
2023-04-15 14:41:43 +0300

    add event

---
+++ main.py
@@ -0,0 +1,30 @@
+import caldav
+from datetime import datetime, timedelta
+from icalendar import Event
+
+
+def add_event(calendar, start=datetime, end=datetime, summary=""):
+    event = calendar.save_event(
+        dtstart=start,
+        dtend=end,
+        summary=summary,
+    )
+
+def get_event(calendar):
+    return
+
+def del_event(calendar):
+    return
+
+
+
+url = "http://tsquared.keenetic.pro:5232/"
+username = "admin"
+password = "admin"
+client = caldav.DAVClient(url, username=username, password=password)
+principal = client.principal()
+
+# Получение списка календарей
+calendar = principal.calendar(name="tinkoffcup")
+add_event(calendar, start=datetime(2023, 5, 13, 5), end=datetime(2023, 5, 13, 9), summary="Test event")
+
