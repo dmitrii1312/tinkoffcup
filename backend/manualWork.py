3fa4ff11312f6863f7a7418df6f24dc73e3175d8
Dmitrii Nechaev <dmitrii_1980@mail.ru>
2023-04-20 14:06:41 -0400

    logic start

--- backend/manualWork.py
+++ backend/manualWork.py
@@ -4,14 +4,13 @@ import time
 import TypeOfWork
 
 @dataclass
-class autoWork(TypeOfWork):
+class manualWork(TypeOfWork):
     compress: int # cтепень сжатия работ
 
     def set_priority(self, priority):
-        if priority == "critical":
-            return False, "Can't set this priority to the task of this type"
-        else:
-            self.priority = priority
+        super().set_priority(priority)
         return True
 
+    def get_priority(self):
+        return super().get_priority()
 
